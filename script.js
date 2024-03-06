f0cecca0ad2dbc46207079d6980115f4cb39689f
Shay <moeshakasanova@gmail.com>
2024-03-06 21:47:35 +0400

    attack logic fucntion

--- script.js
+++ script.js
@@ -169,6 +169,10 @@ function goTown() {
     monsterHealthText.innerText = monsterHealth;
   }
   function attack(){
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power;
 
   }
   function dodge(){
